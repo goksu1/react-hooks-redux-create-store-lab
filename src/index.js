8525c72f9737cd318fb5edbf1b70a82ca016df35
goksu1 <goksu.baykal@gmail.com>
2022-03-03 01:43:56 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+export function createStore(reducer) {
+  let state;
+  function dispatch(action) {
+    state = reducer(state, action)
+  }
+
+  function getState() {
+    return state
+  }
+  return { getState, dispatch }
 }
 
 export function candyReducer(state = [], action) {
@@ -21,6 +29,6 @@ function render() {
     throw new Error("the store's state has not been defined yet");
   }
 }
+  let store = createStore(candyReducer)
 
-// Use your createStore function and the functions provided here to create a store.
-// Once the store is created, call an initial dispatch.
+  store.dispatch({ type: "candies/add" })
